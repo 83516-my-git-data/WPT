972a2aafcddd6878e339db50f7c6a141b8dcd44d
kd2_83516_sahil <sahilchaudhari0821@gmail.com>
2024-05-28 14:50:23 +0530

    Classwork Uploaded

---
+++ Classwork_13/app1/src/App.jsx
@@ -0,0 +1,14 @@
+import Cars from './components/cars'
+import Mobile from './components/mobile'
+import Person from './components/person'
+
+function App() {
+  return (
+    <div>
+      <h1>App Component</h1>
+      <Cars />
+    </div>
+  )
+}
+
+export default App
